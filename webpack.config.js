ded7f654c6ee8d1dacb3a092d164e3b91c789b09
Danya Yamilet <dan.yamilet@hotmail.com>
2024-07-26 12:55:10 -0600

    Estructuras y config del proyecto

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js', //Punto de entrada de la aplicacion
+    output:{
+        filename:'bundle.js', // Nombre del archivo de salida
+        path: path.resolve(__dirname, 'dist'), //Carpeta de salida 
+     },
+     module:{
+        rules:[
+            {
+                test: /\.css$/, //Regex para indentificar archivos CSS
+                use:['style-loader', 'css-loader'], //Loaders para procesar archivos CSS
+            },
+            {
+                test: /\.js$/, // Regex para identificar archivos JS
+                exclude: /node_modules/, //Excluir la carpeta node_modules
+                use: {
+                    loader: 'babel-loader', //Loader para pasar JS moderno a JS mas antiguo
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ] ,
+     },
+     devtool:'source-map', // Generar source maps para facilitar la depuracion 
+     devServer:{
+        contentBase: path.resolve(__dirname,'dist'), // Carpeta principal del servidor 
+        compress: true, //Habilitar la comprension gzip
+        port: 9000, // Puerto del servidor de desarollo 
+     },
+};
+
